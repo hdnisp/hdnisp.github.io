fbaa101e584eca2ebb3876d10b147449b913ef01
hdni <130290615+hdnisp@users.noreply.github.com>
2026-05-13 11:29:19 +0900

    index.js を作成

---
+++ index.js
@@ -0,0 +1,13 @@
+function openSecret1(){
+  const pass = document.getElementById("pass1").value;
+  if(pass === "dream"){
+    document.getElementById("secret1").style.display = "block";
+  }
+}
+
+function openSecret2(){
+  const pass = document.getElementById("pass2").value;
+  if(pass === "22"){
+document.getElementById("secret2").style.display = "block";
+  }
+}
